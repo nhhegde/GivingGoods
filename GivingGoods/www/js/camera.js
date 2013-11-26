095fd60e5c0cc901e8eff8bf8ac9d10de9da5187
nhhegde <neil.f8ed33@gmail.com>
2013-11-26 12:13:37 -0800

    Incorporated Mari's Camera.js and DonationForm.html
    copy-paste

--- GivingGoods/www/js/camera.js
+++ GivingGoods/www/js/camera.js
@@ -56,10 +56,17 @@ var pictureSource;   // picture source
     //
     function capturePhoto() {
       // Take picture using device camera and retrieve image as base64-encoded string
+      cameraPic.src = "images/list1.jpg";
+      $("#takePhotoButton").remove();
       navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
         destinationType: destinationType.DATA_URL });
+      //jnavigator.camera.getPicture(uploadPhoto, null, {sourceType:1,  quality: 50});
     }
 
+
+    function uploadPhoto(data) {
+      cameraPic.src = "data:image/jpeg;base64," + data;
+    }
     // A button will call this function
     //
     function capturePhotoEdit() {
