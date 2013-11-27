0d6dbf1126b5db7acdfddbfd4aeaba178b82eabe
mbatilando <mari.batilando@gmail.com>
2013-11-26 16:21:28 -0800

    Revert "DON'T USE THIS PLEASE!"
    This reverts commit d3dc573d97dd18c9261d4b5a0ed73a8443109a2f.

--- GivingGoods/platforms/android/assets/www/js/camera.js
+++ GivingGoods/platforms/android/assets/www/js/camera.js
@@ -1,33 +1,81 @@
-	var pictureSource;   // picture source
+var pictureSource;   // picture source
     var destinationType; // sets the format of returned value 
-    
+
+    // Wait for Cordova to connect with the device
+    //
     document.addEventListener("deviceready",onDeviceReady,false);
 
+    // Cordova is ready to be used!
+    //
     function onDeviceReady() {
-        pictureSource=navigator.camera.PictureSourceType;
-        destinationType=navigator.camera.DestinationType;
+    	console.log("onDeviceReady has been called")
+        
     }
 
     // Called when a photo is successfully retrieved
     //
     function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64 encoded image data
+      // console.log(imageData);
 
-      var smallImage = document.getElementById('smallImage');
-      smallImage.style.display = 'block';
-      smallImage.src = "data:image/jpeg;base64," + imageData;
+      // Get image handle
+      //
+      var photoButton = document.getElementById('photoButton');
+
+      // Unhide image elements
+      //
+      photoButton.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      photoButton.src = "data:image/jpeg;base64," + imageData;
     }
 
+    // Called when a photo is successfully retrieved
+    //
     function onPhotoURISuccess(imageURI) {
+      // Uncomment to view the image file URI 
+      // console.log(imageURI);
+
+      // Get image handle
+      //
       var largeImage = document.getElementById('largeImage');
+
+      // Unhide image elements
+      //
       largeImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
       largeImage.src = imageURI;
     }
 
+    // A button will call this function
+    //
     function capturePhoto() {
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-        destinationType: destinationType.DATA_URL });
+      // Take picture using device camera and retrieve image as base64-encoded string
+      cameraPic.src = "images/list1.jpg";
+      
+      
+      var options = { quality: 50, 
+      					destinationType: Camera.DestinationType.DATA_URL,sourceType:
+      					 Camera.PictureSource.SAVEDPHOTOALBUM};
+
+ 		navigator.camera.getPicture(onSuccess, onFail, options);
+    //  $("#takePhotoButton").remove();
+    //  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+      //  destinationType: destinationType.DATA_URL });
+      //jnavigator.camera.getPicture(uploadPhoto, null, {sourceType:1,  quality: 50});
     }
 
+
+    function uploadPhoto(data) {
+      cameraPic.src = "data:image/jpeg;base64," + data;
+    }
+    // A button will call this function
+    //
     function capturePhotoEdit() {
       // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
       navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
@@ -38,6 +86,8 @@
     //
     function getPhoto(source) {
       // Retrieve image file location from specified source
+      pictureSource=navigator.camera.PictureSourceType;
+    	destinationType=navigator.camera.DestinationType;
       navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
         destinationType: destinationType.FILE_URI,
         sourceType: source });
@@ -45,6 +95,12 @@
 
     // Called if something bad happens.
     // 
-    function onFail(message) {
-      alert('Failed because: ' + message);
-    }
+    function onSuccess(imageData) {
+
+    	var cameraPic = document.getElementByID("cameraPic");
+ 	   cameraPic.src = "data:image/jpeg;base64," + imageData;
+	}
+
+	function onFail(message) {
+    	alert('Failed because: ' + message);
+	}
